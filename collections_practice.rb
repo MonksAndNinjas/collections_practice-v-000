942e9a21d5c6ca0aa608efd20a4565cdb0baa4c3
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-19 17:15:29 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+   b <=> a
+ end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    if  a == array[1] && b == array[2]
+      1
+    else
+      0
+    end
+  end
+end
+
+def swap_elements_from_to(array, index_1, index_2)
+  swapped_array = []
+  i = 0
+
+  until i == index_1
+    swapped_array << array[i]
+    i += 1
