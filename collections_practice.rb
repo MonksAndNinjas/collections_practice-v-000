0b25384b90c7d40e45c5ceddce31117be0739955
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-19 18:38:23 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,7 +45,7 @@ def swap_elements_from_to(array, index_1, index_2)
 
   i = 0
   swapped_array << array[a]    # array.each |value| if i > b swapped_array << array[i] i += 1.
-  array.each {|value| swapped_array << value i += 1 if i > b end i += 1}
+  array.each {|value| swapped_array << value if i > b end i += 1}
    swapped_array
 end
 
