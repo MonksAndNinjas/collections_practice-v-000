ae568a691f93f853716f9442cb13683b80a1588b
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-19 18:08:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,7 +48,11 @@ def swap_elements_from_to(array, index_1, index_2)
 
   i = 0
   swapped_array << array[a]    # array.each |value| if i > b swapped_array << array[i] i += 1.
-  array.each {|value| swapped_array << value and i += 1 if i > b else i += 1}
+  array.each do |value|
+     if i > b
+       swapped_array << value and i += 1
+     else i += 1
+     end
 end
 
 def reverse_array(array)
